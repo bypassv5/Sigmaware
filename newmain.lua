cd532c7cafb13f9654d2a7bfd56e22c73f619ab8
bypassv5 <163147680+bypassv5@users.noreply.github.com>
2024-12-08 16:09:05 -0500

    Update newmain.lua

--- newmain.lua
+++ newmain.lua
@@ -1,1 +1,7 @@
-
+if game.PlaceId == 8542259458 then
+  return loadstring(game:HttpGet("https://raw.githubusercontent.com/bypassv5/Sigmaware/main/games/8542259458.lua"))()
+elseif game.PlaceId == 1 then
+  return loadstring(game:HttpGet("https://raw.githubusercontent.com/bypassv5/Sigmaware/main/main.lua"))()
+else
+  return loadstring(game:HttpGet("https://raw.githubusercontent.com/bypassv5/Sigmaware/main/universal.lua"))()
+end
